125d708fb0605e23b89338ca0aea39f8b88d1075
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-08-29 01:49:08 -0600

    complete lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,30 @@
 def badge_maker(name):
-    return None
+    return (f"Hello, my name is {name}.")
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for name in names:
+        badges.append(f"Hello, my name is {name}.")
+    return badges
 
-def assign_rooms(names):
-    return None
+def assign_rooms(speakers):
+    rooms = [None] * 8
+    room_assignments = []
+
+    for i in range(len(speakers)):
+        if i < 8:
+            speaker = speakers[i]
+            rooms[i] = speaker
+            room_number = i + 1
+            assignment_message = f"Hello, {speaker}! You'll be assigned to room {room_number}!"
+            room_assignments.append(assignment_message)
+
+    return room_assignments
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    for badge in badges:
+        print(badge)
+    room_assignments = assign_rooms(names)
+    for assignment in room_assignments:
+        print(assignment)
